78fdab13a41389a89ac28b98eba1d3e2224ec0c7
Jonathan <joproulx@hotmail.com>
2018-08-03 22:57:46 -0400

    Added logtraill container

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -13,6 +13,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import LogTrail from 'containers/LogTrail/LogTrail';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 import './style.scss';
@@ -29,6 +30,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/features" component={FeaturePage} />
+      <Route path="/logtrail" component={LogTrail} />
       <Route path="" component={NotFoundPage} />
     </Switch>
     <Footer />
---
+++ app/containers/LogTrail/LogTrail.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDataGrid from 'react-data-grid';
+import { Helmet } from 'react-helmet';
+import './style.scss';
+
+export default class LogTrail extends React.Component {
+   // eslint-disable-line react/prefer-stateless-function
+   constructor(props, context) {
+    super(props, context);
+    this.createRows();
+    this._columns = [
+      { key: 'id', name: 'ID' },
+      { key: 'title', name: 'Title' },
+      { key: 'count', name: 'Count' } ];
+
+    this.state = null;
+  }
+   createRows = () => {
+    let rows = [];
+    for (let i = 1; i < 1000; i++) {
+      rows.push({
+        id: i,
+        title: 'Title ' + i,
+        count: i * 1000
+      });
+    }
+
+    this._rows = rows;
+  };
+
+  rowGetter = (i) => {
+    return this._rows[i];
+  };
+
+  render() {
+    return  (
+      <ReactDataGrid
+        columns={this._columns}
+        rowGetter={this.rowGetter}
+        rowsCount={this._rows.length}
+        minHeight={500} />);
+  }
+}
+
+
